11558d62e3ef9b6c28c75d766aff9e2ca21ce353
OXFAM\felixs <fsingletonthorn@gmail.com>
2024-05-11 16:31:39 +1000

    Loop through total number of records in query

--- requests/requests_test.py
+++ requests/requests_test.py
@@ -2,11 +2,11 @@ import json
 import requests
 import re, string, timeit
 import time
+import yaml
 import pandas as pd 
 import logging
 import math
 from requests.adapters import HTTPAdapter, Retry
-
 
 s = requests.Session()
 
@@ -16,7 +16,17 @@ retries = Retry(total=5,
 s.mount('http://', HTTPAdapter(max_retries=retries))
 
 # Grabbing access token
-response = s.post('https://auth.domain.com.au/v1/connect/token', data = {'client_id':'client_id',"client_secret":"client_secret","grant_type":"client_credentials","scope":"api_listings_read","Content-Type":"text/json"})
+with open('client_credentials.yaml', 'r') as f:
+    doc = yaml.load(f, Loader=yaml.Loader)
+
+response = s.post('https://auth.domain.com.au/v1/connect/token', 
+                  data = {'client_id':doc[0]['client_id'],
+                          "client_secret":doc[0]['client_secret'],
+                          "grant_type":"client_credentials",
+                          "scope":"api_listings_read",
+                          "Content-Type":"text/json"}
+                )
+
 token=response.json()
 access_token=token["access_token"]
 
@@ -28,7 +38,6 @@ access_token=token["access_token"]
 # request = s.get(url,headers=auth)
 # r=request.json()
 
-
 # Looping through available records
 listings = []
 state = 'VIC'
@@ -36,7 +45,8 @@ auth = {"Authorization":"Bearer "+access_token}
 postCode = '3058'
 pageNumber = 0
 url = "https://api.domain.com.au/v1/listings/residential/_search" # Set destination URL here
-    
+updating = True
+
 while updating:
 
     pageNumber  += 1
@@ -65,34 +75,28 @@ while updating:
 
     # Counting number of pages required here
     if pageNumber == 1:
+        
+        # Calculations for number of loops
         page_size = pd.to_numeric(request.headers['X-Pagination-PageSize'])
         num_records = pd.to_numeric(request.headers['X-Total-Count'])
         total_pages = math.ceil(num_records / page_size)
-
+        # Initializing data store
+        listings = request.json()
     
+    else:
+        listings.extend(request.json())
+
+    print('Page ' + str(pageNumber) + ' of ' + str(total_pages) + ' (' + str(len(listings)) + ' of ' + str(num_records) + ' total records downloaded)')
     # Check x-total count, if greater than 1000 narrow search params
     # Check x-total count and iterate through pages if required
-
-
-    l=request.json()
     
-    for listing in l:
-        listings.append(listing["listing"]["id"])
-    listings
+    time.sleep(0.15)  # sleep a bit so you don't make too many API calls too quickly  ~ this should prevent us from using more than 10 in a second
+    updating = pageNumber < total_pages
 
-    
-    if int(property_id) in listings:
-            max_price=max_price-increment
-            print("Lower bound found: ", max_price)
-            updating=False
-    else:
-        max_price=max_price+increment
-        print("Not found. Increasing max price to ",max_price)
-        time.sleep(0.1)  # sleep a bit so you don't make too many API calls too quickly  
 
 request = requests.get(url,headers=auth)
 r=request.json()
-test = pd.json_normalize(l)
+test = pd.json_normalize(listings)
 test.columns
 out = requests.post(url = 'https://api.domain.com.au/v1/listings/residential/_search',
     headers = {'accept': 'application/json',
