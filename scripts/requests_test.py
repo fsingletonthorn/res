d23c86a8566af981239f25ca1da63ec843a5712e
OXFAM\felixs <fsingletonthorn@gmail.com>
2024-06-21 13:24:37 +1000

    Backup w/ requests test inside loop

--- scripts/requests_test.py
+++ scripts/requests_test.py
@@ -1,6 +1,6 @@
 #import json
 import requests
-import datetime
+import datetime as dt
 #import re, string, timeit
 import time
 #import yaml
@@ -29,73 +29,110 @@ state = 'Vic'
 region = ''
 area = ''
 listing_type = 'Sale'
-download_date = datetime.datetime.today().isoformat()
-updated_since = (datetime.datetime.today() - datetime.timedelta(days=1)).isoformat()
+updated_since = ''#(dt.datetime.today() - dt.timedelta(days=10)).isoformat()
+listed_since = (dt.datetime.today() - dt.timedelta(days=30)).isoformat()
 
- # Initialising some things
-updating = True
-pageNumber = 0
 
-while updating:
-    pageNumber  += 1
+def residential_listings_search_loop(access_token = None, 
+                                    request_session = None,
+                                    postcode = None,
+                                    state = None,
+                                    region = None,
+                                    area = None,
+                                    listing_type = None,
+                                    updated_since = None,
+                                    listed_since= None): 
+    # Initialising variables
+    updating = True
+    pageNumber = 0
+    download_date = dt.datetime.today().isoformat()
 
-    request = residential_listings_search(
-                                access_token = access_token, 
-                                request_session = s,
-                                page_number = pageNumber,
-                                postcode = postcode,
-                                state = state, 
-                                region = region,
-                                area = area, 
-                                listing_type = listing_type, 
-                                updated_since = updated_since
-                                )
-    
-    # Error out if status code not 200
-    if request.status_code != 200:
-        raise ValueError('Error: Request status code request was: ' + str(request.status_code) + ', not 200')
+    while updating:
+        pageNumber  += 1
 
-        # Checking that daily quotas have not been reached
-    if pd.to_numeric(request.headers['X-Quota-PerDay-Remaining']) == 1:
-        raise ValueError('Error: ' + request.headers['X-Quota-PerDay-Remaining'] + ' quota per day remaining, no more queries left')     
-    ## Later, rather than cancelling, return one last result, add a log event, and go through the last event    
-
-    # Counting number of pages required here
-    if pageNumber == 1:
+        request = residential_listings_search(
+                                    access_token = access_token, 
+                                    request_session = s,
+                                    page_number = pageNumber,
+                                    postcode = postcode,
+                                    state = state, 
+                                    region = region,
+                                    area = area, 
+                                    listing_type = listing_type, 
+                                    updated_since = updated_since,
+                                    listed_since= listed_since
+                                    )
         
-        # Calculations for number of loops
-        page_size = pd.to_numeric(request.headers['X-Pagination-PageSize'])
-        num_records = pd.to_numeric(request.headers['X-Total-Count'])
-        total_pages = math.ceil(num_records / page_size)
+        # Error out if status code not 200
+        if request.status_code != 200:
+            raise ValueError('Error: Request status code request was: ' + str(request.status_code) + ', not 200')
 
-        # Checking that we are not going beyond the pagination limit
-        if total_pages > 10:
-            raise ValueError('Error: Request returns: ' + str(num_records) + ', total records, narrow search parameters to only include 1000 listings at most to include all listings')
+        # Counting number of pages required here
+        if pageNumber == 1:
+            
+            # Calculations for number of loops
+            page_size = pd.to_numeric(request.headers['X-Pagination-PageSize'])
+            num_records = pd.to_numeric(request.headers['X-Total-Count'])
+            total_pages = math.ceil(num_records / page_size)
 
-        # Checking that we are not going beyond the pagination limit
-        if num_records == 0:
-            raise ValueError('Error: Request returns: ' + str(num_records) + ', check search parameters')
- 
-    # Getting Json responses
-    request_json = request.json()  
+            # Checking that we are not going beyond the pagination limit
+        # if total_pages > 10:
+        #     raise ValueError('Error: Request returns: ' + str(num_records) + ', total records, narrow search parameters to only include 1000 listings at most to include all listings')
 
-    # Saving or extending json
-    if pageNumber == 1:
-        listings = request_json
-    else:
-        listings.extend(request_json)
-
-    print('Page ' + str(pageNumber) + ' of ' + str(total_pages) + ' (' + str(len(listings)) + ' of ' + str(num_records) + ' total records downloaded)')
-    # Check x-total count, if greater than 1000 narrow search params
-    # Check x-total count and iterate through pages if required
+            # Checking that we are not going beyond the pagination limit
+            if num_records == 0:
+                raise ValueError('Error: Request returns: ' + str(num_records) + ' records, check search parameters')
     
-    # sleep a bit so you don't make too many API calls too quickly  ~ this should prevent us from sending more than 10 requests in a second
-    time.sleep(0.4)  
-    updating = pageNumber < total_pages
+        # Getting Json responses
+        request_json = request.json()  
 
-    # Inserting downloaded date
-    for i in range(len(listings)): 
-        listings[i]['download_date'] = download_date
+        # Saving or extending json
+        if pageNumber == 1:
+            listings = request_json
+        else:
+            listings.extend(request_json)
+
+        print('Page ' + str(pageNumber) + ' of ' + str(total_pages) + ' (' + str(len(listings)) + ' of ' + str(num_records) + ' total records downloaded)')
+        # Check x-total count, if greater than 1000 narrow search params
+        # Check x-total count and iterate through pages if required
+        
+        # sleep a bit so you don't make too many API calls too quickly  ~ this should prevent us from sending more than 10 requests in a second
+        time.sleep(0.4)  
+        updating = pageNumber < total_pages
+
+        # Inserting downloaded date
+        for i in range(len(listings)): 
+            listings[i]['download_date'] = download_date
+
+        if pageNumber == 10:
+            break
+
+                    # Checking that daily quotas have not been reached
+        if pd.to_numeric(request.headers['X-Quota-PerDay-Remaining']) == 0:
+            print('X-Quota-PerDay-Remaining = 0')
+            break
+
+    meta = {'updated_since': updated_since,
+             'listed_since': listed_since, 
+             'daily_quota_remaining': pd.to_numeric(request.headers['X-Quota-PerDay-Remaining']),
+             'pages_remaining': total_pages - pageNumber
+             }
+
+    out = {'listings': listings, 'meta': meta}
+
+    return(out)
+
+listings_w_meta = residential_listings_search_loop(access_token = access_token, 
+                                    request_session = s,
+                                    postcode = postcode,
+                                    state = state, 
+                                    region = region,
+                                    area = area, 
+                                    listing_type = listing_type, 
+                                    updated_since = updated_since,
+                                    listed_since= listed_since)
+
+listings = listings_w_meta['listings']
 
 # Now dealing with the nested data
 listings_json = pd.json_normalize(listings)
@@ -128,7 +165,11 @@ all_listings.columns = (all_listings.columns
                 .str.replace('.', '_', regex=False)
              )
 
-all_listings.columns
-# example write
-all_listings.to_csv(f'data/all_listings_vic_{}-{updated_since}.csv')
+max_listed = pd.to_datetime(listings_json['listing.dateListed']).max().isoformat()
 
+previous_listed_since = listed_since
+
+# example write
+all_listings.to_csv(f'data/all_listings_vic_{previous_listed_since}-{max_listed}.csv')
+
+listed_since = max_listed
