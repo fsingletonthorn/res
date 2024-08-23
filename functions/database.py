338680ffa853f7a7b54f7100a9b6902c17008cec
OXFAM\felixs <fsingletonthorn@gmail.com>
2024-08-23 17:21:17 +1000

    Moving update data script into a function

--- functions/database.py
+++ functions/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import datetime as dt
 
 # Grab last uploaded date: 
 ## Get parameters
@@ -16,3 +17,38 @@ def get_last_download_metadata(database_loc):
     out = pd.read_sql('SELECT * FROM download ORDER BY id DESC LIMIT 1;', conn)
     conn.close()
     return(out)
+
+def update_listings_tables(raw_output, cleaned_listings):
+    conn = sqlite3.connect("res_database.db")
+    cur = conn.cursor()
+
+    download_meta = pd.DataFrame({
+        'download_date': [dt.datetime.today().isoformat()], 
+        'listed_since_date': [raw_output['listed_since_date']],
+        'max_listed_since_date': [raw_output['max_listed_since_date']],
+        'postcode': [raw_output['postcode']],
+        'state': [raw_output['state']],
+        'region': [raw_output['region']],
+        'area': [raw_output['area']],
+        'listing_type': [raw_output['listing_type']],
+        'updated_since': [raw_output['updated_since']],
+        'pages_remaining': [raw_output['pages_remaining']]
+        })
+
+    download_meta.to_sql('download', conn, if_exists='append', index=False)
+
+    # pd.read_sql('select * from download;', conn)
+
+    max_download_id = pd.read_sql('select max(id) as download_id from download;', conn)
+
+    ## Striping object types of
+    all_listings_for_upload = cleaned_listings.convert_dtypes().infer_objects()
+    all_listings_for_upload.update(all_listings_for_upload.select_dtypes('object').astype(str))
+    all_listings_for_upload['download_id'] = max_download_id['download_id'][0]
+
+    all_listings_for_upload.to_sql('raw_listing', conn, if_exists='append', index=False)
+
+    conn.execute("VACUUM")
+    conn.commit()
+
+    cur.close()
--- scripts/requests_test.py
+++ scripts/requests_test.py
@@ -10,7 +10,7 @@ import math
 import sqlite3
 from requests.adapters import HTTPAdapter, Retry
 from functions.domain import get_domain_access_token,residential_listings_search
-from functions.database import get_last_download_metadata
+from functions.database import get_last_download_metadata, update_listings_tables
 from functions.helper_functions import clean_listings
 
 ## Session setup
@@ -46,42 +46,10 @@ output = residential_listings_search(access_token = access_token,
 
 all_listings = clean_listings(output)
 
-listed_since = output['listed_since_date']
-max_listed_since_date = output['max_listed_since_date']
+if len(all_listings) > 0:
+    update_listings_tables(output, all_listings)
 
-# example write
-all_listings.to_csv(f'data/all_listings_vic_{dt.date.today().isoformat()}.csv', index = False)
+print(f'{len(all_listings)} records added to raw listing tables, {output["pages_remaining"]}  pages remaining after download completed.')
 
-conn = sqlite3.connect("res_database.db")
-cur = conn.cursor()
-
-download_meta = pd.DataFrame({
-    'download_date': [dt.datetime.today().isoformat()], 
-    'listed_since_date': [output['listed_since_date']],
-    'max_listed_since_date': [output['max_listed_since_date']],
-    'postcode': [output['postcode']],
-    'state': [output['state']],
-    'region': [output['region']],
-    'area': [output['area']],
-    'listing_type': [output['listing_type']],
-    'updated_since': [output['updated_since']],
-    'pages_remaining': [output['pages_remaining']]
-    })
-
-download_meta.to_sql('download', conn, if_exists='append', index=False)
-
-# pd.read_sql('select * from download;', conn)
-
-max_download_id = pd.read_sql('select max(id) as download_id from download;', conn)
-
-## Striping object types of
-all_listings_for_upload = all_listings.convert_dtypes().infer_objects()
-all_listings_for_upload.update(all_listings_for_upload.select_dtypes('object').astype(str))
-all_listings_for_upload['download_id'] = max_download_id['download_id'][0]
-
-all_listings_for_upload.to_sql('raw_listing', conn, if_exists='append', index=False)
-
-conn.execute("VACUUM")
-conn.commit()
-
-cur.close()
+# Writing to csv
+## all_listings.to_csv(f'data/all_listings_vic_{dt.date.today().isoformat()}.csv', index = False)
